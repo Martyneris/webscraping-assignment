a89ccec3a31d9d47fb59eb7093f8debd8723aab0
Martynas Šiška <33836575+Martyneris@users.noreply.github.com>
2018-09-23 21:05:53 +0300

    added commentary lines

--- sample.js
+++ sample.js
@@ -8,12 +8,12 @@ casper.start('https://classic.flysas.com').viewport(1200,1000);
 casper.wait(3000, function(){
     this.echo(this.getTitle());
 })
-
+// closing the popup
 casper.then(function() {
     this.click('.closeMarketSelectorLeft');
     console.log('button clicked')
 });
-
+// choosing departure date
 casper.then(function() {
     this.click('.flOutDate');
     console.log('button clicked')
@@ -28,7 +28,7 @@ casper.then(function() {
     this.click('#ui-datepicker-div table tbody tr:nth-child(2) td:nth-child(1) a');
     console.log('button clicked')
 });
-
+// choosing arrival date
 casper.then(function() {
     this.click('.flInDate');
     console.log('button clicked')
@@ -38,25 +38,24 @@ casper.then(function() {
     this.click('#ui-datepicker-div table tbody tr:nth-child(2) td:nth-child(7) a');
     console.log('button clicked')
 });
-
+// entering departure and arrival airports information into the form and submitting it
 casper.thenEvaluate(function(depAirport,arrAirport) {
     document.querySelector('input[name="ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenFrom"]').setAttribute('value', depAirport);
     document.querySelector('input[name="ctl00$FullRegion$MainRegion$ContentRegion$ContentFullRegion$ContentLeftRegion$CEPGroup1$CEPActive$cepNDPRevBookingArea$predictiveSearch$hiddenTo"]').setAttribute('value', arrAirport);
-    // document.querySelector('form[name="aspnetForm"]').submit();
 }, 'ARN','LHR');
-
+// making a screenshot to check the website status after the info input
 casper.wait(5000, function(){
     casper.capture('screenshots/beforesearchpress.png');
     console.log('captured')
 })
-
+// pressing the search button to submit all entered info and proceed to another page
 casper.then(function() {
     // this.click('#ctl00_FullRegion_MainRegion_ContentRegion_ContentFullRegion_ContentLeftRegion_CEPGroup1_CEPActive_cepNDPRevBookingArea_Searchbtn_ButtonLink')
     casper.click(x('//*[@id="ctl00_FullRegion_MainRegion_ContentRegion_ContentFullRegion_ContentLeftRegion_CEPGroup1_CEPActive_cepNDPRevBookingArea_Searchbtn_ButtonLink"]'))
     console.log('search button clicked')
 });
 
-
+// checking the current URL and getting the HTML of the page for later reference when manipulating other buttons and elements 
 casper.then(function(){
     casper.wait(15000, function(){
         console.log('clicked ok, new location is ' + this.getCurrentUrl());
@@ -70,17 +69,17 @@ casper.then(function(){
         })
     })
 });
-
+// clicking on first captcha button
 casper.then(function() {
     this.click('.geetest_radar_tip_content');
     console.log('captcha clicked')
 });
-
+// checking status after 5s 
 casper.wait(5000, function(){
     casper.capture('screenshots/after.png');
     console.log('captured')
 })
-
+// the following functions were my attempts of "drag and drop" captcha solving to proceed further
 casper.then(function(){
     this.mouseEvent('click', '.geetest_slider', "20%", "50%");
 })
@@ -133,10 +132,11 @@ var triggerDragAndDrop = function (selectorDrag, selectorDrop) {
 
     return true;
   };
-
+// simulate drag and drop to solve captcha puzzle
   casper.then(function triggerDragAndDrop(selectorDrag, selectorDrop) {
     }, '.geetest_div_slice', '.geetest_div_fullbg');
 
+// screenshot for status check
   casper.wait(2000, function(){
         casper.capture('screenshots/afterdrag.png');
         console.log('captured')
@@ -145,6 +145,7 @@ var triggerDragAndDrop = function (selectorDrag, selectorDrop) {
 //   casper.then(function(){
 //       this.echo(this.getCurrentUrl());
 //   })
+
 casper.then(function(){
     this.echo(this.getHTML());
 })
